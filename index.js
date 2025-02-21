6fa8d864c46e46b782133847316e8f38802d66c3
zaZZa564 <anus_bobra564@mail.ru>
2025-02-21 21:45:54 +0300

    cowsay global usage

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 let num = 5;
 console.log(num);
+// cowsay JavaScript FTW!
+// $ cowsay JavaScript FTW!
+//  _________________
+// < JavaScript FTW! >
+//  -----------------
+//         \   ^__^
+//          \  (oo)\_______
+//             (__)\       )\/\
+//                 ||----w |
+//                 ||     ||
